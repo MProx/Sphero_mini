849930e8173670fe362ba48706f0fdf4e6863f4c
MProx <34684113+MProx@users.noreply.github.com>
2018-05-19 19:31:08 +0200

    Update sphero_mini.py

--- sphero_mini.py
+++ sphero_mini.py
@@ -6,10 +6,10 @@ import time
 class sphero_mini():
     def __init__(self, MACAddr):
         '''
-        initialize class instance and then build list of BLE sevices and characteristics.
+        initialize class instance and then build dictionaries of BLE sevices and characteristics.
         Also sends text string to Anti-DOS characteristic to prevent returning to sleep.
         '''
-        self.sequence = 0
+        self.sequence = 1
 
         self.p = Peripheral(MACAddr, "random") #connect
 
@@ -18,14 +18,17 @@ class sphero_mini():
                     "Nordic DFU Service": self.p.getServiceByUUID("00020001574f4f2053706865726f2121")}
         self.characteristics = dict(zip(characteristicNames, self.p.getCharacteristics()))
 
-        # The following command seems to prevent the sphero mini from going to sleep again after 10 seconds
+        # The following command seems be necessary to prevent the sphero mini from going to sleep again after 10 seconds
         self.characteristics["Anti DOS"].write("usetheforce...band".encode())
+    def disconnect(self):
+        self.p.disconnect()
 
     def _send(self, characteristic=None, devID=None, commID=None, payload=[]):
         '''
         A generic "send" method, which will be used by other methods to send a command ID, payload and
         appropriate checksum to a specified device ID. Mainly useful because payloads are optional,
-        and can be of varying length, and packets must be converted to binary. For internal use only.
+        and can be of varying length, to convert packets to binary, and calculate and send the
+        checksum. For internal use only.
         '''
         self.sequence += 1 # Increment sequence number (I am not sure that this is necessary, but probably good practice)
         sendBytes = [sendPacketConstants["StartOfPacket"],
@@ -51,6 +54,7 @@ class sphero_mini():
 
         #send to specifiec characteristic:
         characteristic.write(output)
+        time.sleep(0.1) # short pause because it seems that successive commands collide with each other
 
     def wake(self):
         '''
@@ -82,3 +86,36 @@ class sphero_mini():
                   devID = deviceID['userIO'],
                   commID = userIOCommandIDs["allLEDs"],
                   payload = [0x00, 0x70, red, green, blue]) #no idea where the 0x00 or 0x70 come from - borrowed from igbopie's code
+
+    def roll(self, speed=None, heading=None):
+        '''
+        Start to move the Sphero at a given direction and speed.
+        heading: integer from 0 - 360 (degrees)
+        speed: Integer from 0 - 255
+
+        NOTE: Aiming has not yet been implemented, so the Sphero will likely begin to move
+        in a seemingly random direction (not actuallly random because the device probably wasn't
+        pointing at zero degrees when you started)
+        '''
+        if speed < 0 or speed > 255:
+            print("WARNING: roll speed argument outside of allowed range")
+
+        speedH = (speed & 0xFF00) >> 8
+        speedL = speed & 0xFF
+        headingH = (heading & 0xFF00) >> 8
+        headingL = heading & 0xFF
+        self._send(characteristic = self.characteristics["API V2"],
+                  devID = deviceID['driving'],
+                  commID = drivingCommands["driveWithHeading"],
+                  payload = [speedL, speedH, headingL, headingH])
+
+    def setBackLEDIntensity(self, brightness=None):
+        '''
+        Set device LED backlight intensity based on 0-255 values
+
+        NOTE: this is not the same as aiming - it only turns on the LED
+        '''
+        self._send(characteristic = self.characteristics["API V2"],
+                  devID = deviceID['userIO'],
+                  commID = userIOCommandIDs["allLEDs"],
+                  payload = [0x00, 0x01, brightness]) #no idea where the 0x00 or 0x70 come from - borrowed from igbopie's code
