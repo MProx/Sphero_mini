c951077c29da1de0005befa23cf985cd31dc3e9d
Andrew Trageser <andrew.trageser@gmail.com>
2020-01-19 16:25:00 -0800

    pass

--- sphero_mini_controller/controllable.py
+++ sphero_mini_controller/controllable.py
@@ -72,7 +72,7 @@ class ControllableSphero(TraceableSphero):
         # self.device.set_motion_timeout(self._motion_timeout)
         # self.device.set_rgb(0xFF, 0xFF, 0xFF, True)
         # self.device.setLEDColor(red=255, green=0, blue=0)
-
+        pass
         # self._configure_sensor_streaming()
 
         # # Collision detection
